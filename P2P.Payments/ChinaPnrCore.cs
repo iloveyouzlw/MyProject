ee10e38f578bede01430ca31e76f84b9bd5fcd4e
iloveyouzlw <baiduzhanglingwei@qq.com>
2014-11-01 00:39:58 +0800

    今晚提交一下

---
+++ P2P.Payments/ChinaPnrCore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2P.Payments
+{
+    /// <summary>
+    /// 汇付天下
+    /// </summary>
+    public class ChinaPnrCore
+    {
+
+    }
+}
